e66d3ac53369bedcd53511d09a8efa864a4cb9e4
huangfeng15 <876033814@qq.com>
2025-03-25 11:55:15 +0800

    Delete bootstrap.py

--- bootstrap.py
+++
@@ -1,9 +0,0 @@
-import os, sys 
-if getattr(sys, 'frozen', False): 
-    os.chdir(os.path.dirname(sys.executable)) 
-else: 
-    os.chdir(os.path.dirname(os.path.abspath(__file__))) 
-from main import PDFExtractorGUI 
-if __name__ == "__main__": 
-    app = PDFExtractorGUI() 
-    app.root.mainloop() 
